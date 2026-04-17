a810db6e8a394fee1f84d4e036347660d52dcf6a
KasibM <129322185+KasibM@users.noreply.github.com>
2026-04-17 13:29:44 -0400

    changed redirect to login

--- restricted.php
+++ restricted.php
@@ -5,9 +5,8 @@
 <body>
     <main>
         <h1> Sorry, you must be logged into view this content! </h1>
-        <a href="index.php"> Back to Home Page </a>
+        <a href="login.php"> Login </a>
     </main>
 </body>
 
 </html>
-<?php require_once 'includes/footer.php' ?>
