217df8fd0027c806033ebd3c1f42281f638e266a
KasibM <129322185+KasibM@users.noreply.github.com>
2026-04-17 12:22:12 -0400

    Initialise with resources from my main repository

---
+++ restricted.php
@@ -0,0 +1,14 @@
+<?php 
+//From Lesson 10
+require_once 'includes/header.php'
+?>
+
+<body>
+    <main class="container restricted text-center">
+        <h1> Sorry, you must be logged into view this content! </h1>
+        <a href="index.php" class="btn btn-secondary"> Back to Home Page </a>
+    </main>
+</body>
+
+</html>
+<?php require_once 'includes/footer.php' ?>
---
+++ sql/images.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS images;
+CREATE TABLE images (
+  
+); 
